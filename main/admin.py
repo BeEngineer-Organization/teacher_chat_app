c607ea989ebff7040dfe2f14cfc1f4230c341b7e
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2025-11-27 19:36:34 +0900

    admin.py修正

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Talk, Reply
 
 admin.site.register(User)
+admin.site.register(Talk)
+admin.site.register(Reply)
